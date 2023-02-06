3dfcf808830890a8009676349102f4510661b411
Meraj Ali <96920684+MerajAli1@users.noreply.github.com>
2023-02-06 21:07:35 +0500

    first commit

---
+++ New folder (3)/app.js
@@ -0,0 +1,77 @@
+//1. Check if a number is odd or even in JavaScript
+// var num=prompt("Enter Number: ")
+// if (num%2==0){
+//         document.write(num+" "+"Is Even")
+//     }
+// else{
+//     document.write(num+" "+"Is Odd")
+// }
+
+
+//2. Check if input variable is a number or not
+// var userInp = +prompt("Enter Something...")
+// var n;
+// if (userInp == n) {
+//     document.write(userInp + " " + "is Number")
+// }
+// else {
+//     document.write(userInp + " " + "is not Number")
+// }
+
+
+// 3. Find the largest of two number
+// var num1 = +prompt("Enter Number 1")
+// var num2 = +prompt("Enter Number 2")
+// if (num1 > num2) {
+//     document.write(num1 + " " + "is largest")
+// }
+// else if (num2 > num1) {
+//     document.write(num2 + " " + "is largest")
+// }
+// else if (num2 == num1) {
+//     document.write(num1 + " and " + num2 + " " + "are equal")
+// }
+// else {
+//     document.write("Invalid")
+
+// }
+
+
+// 4. Find the largest of three number
+// var num1 = +prompt("Enter Number 1")
+// var num2 = +prompt("Enter Number 2")
+// var num3 = +prompt("Enter Number 3")
+// if (num1 > num2 && num1 > num3) {
+//     document.write(num1 + " " + "is largest")
+// }
+// else if (num2 > num1 && num2 > num3) {
+//     document.write(num2 + " " + "is largest")
+// }
+// else if (num3 > num1 && num3 > num2) {
+//     document.write(num3 + " " + "is largest")
+// }
+// else if (num1 == num2 && num2 == num3 && num3 == num1) {
+//     document.write(num1+ ", " + num2 + " and " + num3 + " " + "are equal")
+// }
+// else {
+//     document.write("Invalid")
+
+// }
+
+
+// 5. Check if a triangle is equilateral, scalene, or isosceles
+// var num1 = +prompt("Emter Lenght of first side")
+// var num2 = +prompt("Emter Lenght of second side")
+// var num3 = +prompt("Emter Lenght of third side")
+
+// if (num1 == num2 && num2 == num3 && num3 == num1) {
+//     document.write(num1 + ", " + num2 + " and " + num3 + " " + "is Equal triangle")
+// }
+// else if (num1 == num2 && num2 != num3) {
+//     document.write(num1 + ", " + num2 + " and " + num3 + " " + "is isosceles triangle")
+    
+// }
+// else {
+//     document.write(num1 + ", " + num2 + " and " + num3 + " " + "is scalene triangle")
+// }
+
